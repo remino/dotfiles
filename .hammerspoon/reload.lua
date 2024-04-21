3811442f81270beda8b284cb4aa2a0359f768f9f
Rémino Rem <29999+remino@users.noreply.github.com>
2024-04-22 06:42:05 +0900

    Adjust Hammerspoon reload

--- .hammerspoon/reload.lua
+++ .hammerspoon/reload.lua
@@ -2,6 +2,7 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 hs.hotkey.bind({"cmd", "shift", "ctrl"}, "R", function()
-	hs.fnutils.partial(hs.reload, self)
+	print("HammerSpoon Config Reloaded")
+	hs.reload()
 	hs.alert.show("HammerSpoon Config Reloaded")
 end)
