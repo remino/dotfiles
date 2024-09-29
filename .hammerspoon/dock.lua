d140fff78602aee5a19199d775c4f3f0962bc132
Rémino Rem <29999+remino@users.noreply.github.com>
2024-09-29 17:19:03 +0900

    Add dock show() and hide()

--- .hammerspoon/dock.lua
+++ .hammerspoon/dock.lua
@@ -12,3 +12,45 @@ function focusDockApp(num)
 
 	moveMouseToFocusedWindow()
 end
+
+autoHide = false
+dock = {}
+
+function dock.show()
+	ok, result = hs.osascript.applescript([[
+		tell application "System Events"
+			tell dock preferences
+				get autohide
+			end tell
+		end tell
+	]])
+
+	if not ok then
+		hs.alert.show("Error showing dock")
+		return
+	end
+
+	autoHide = result
+
+	hs.osascript.applescript([[
+		tell application "System Events"
+			tell dock preferences
+				set autohide to false
+			end tell
+		end tell
+	]])
+end
+
+function dock.hide()
+	if not autoHide then
+		return
+	end
+
+	hs.osascript.applescript([[
+		tell application "System Events"
+			tell dock preferences
+				set autohide to true
+			end tell
+		end tell
+	]])
+end
--- .hammerspoon/urlevent.lua
+++ .hammerspoon/urlevent.lua
@@ -1,3 +1,15 @@
 hs.urlevent.bind("focusDockApp", function(eventName, params)
 	focusDockApp(tonumber(params["n"]))
 end)
+
+hs.urlevent.bind("dock", function(eventName, params)
+	if params["action"] == "hide" then
+		dock.hide()
+		return
+	end
+
+	if params["action"] == "show" then
+		dock.show()
+		return
+	end
+end)
